c9818406618d0ce0973d3890c7b2b2d362d6d011
@hexlet/cli <support@hexlet.io>
2023-04-08 15:37:36 +0300

    submit rails-basics-ru/rack

--- rails-basics-ru/rack/lib/app/router.rb
+++ rails-basics-ru/rack/lib/app/router.rb
@@ -5,7 +5,16 @@ require 'rack'
 class Router
   def call(env)
     # BEGIN
-    
+    req = Rack::Request.new(env)
+
+    case req.path_info
+    when '/'
+      [200, { 'Content-Type' => 'text/html' }, ['Hello, World!']]
+    when '/about'
+      [200, { 'Content-Type' => 'text/html' }, ['About page']]
+    else
+      [404, { 'Content-Type' => 'text/html' }, ['404 Not Found']]
+    end
     # END
   end
 end
